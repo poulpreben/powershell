2e6a484de71c79c5a4fc3b63b7e080441d1fb044
Preben Berg <preben@uhrskov.biz>
2016-06-28 07:15:49 +0200

    Updated VeeamPrnxCacheControl.ps1 with changes from James Smith. Thanks!

--- README.md
+++ README.md
@@ -2,17 +2,49 @@
 This is a repository for mixed PowerShell scripts I have created over time.
 
 ## Veeam/PernixData Cache Control
+* Contributors
+  * James Smith - PernixData
+  * Andy Daniel - PernixData
+* Created: 11/02/2015
+* Last Updated: v9.1 - 01/28/2016
+* v9.1 Update - Documentation changes and script cleanup. No new functionality.
 
-This script enables integration between Veeam Backup & Replication and PernixData FVP to allow FVP write back enabled VMs associated with the Veeam backup or replication job to be transitioned to write through before the job runs. Conversely, it also will transition the VMs back to the previous write back state with the correct number of peers.
+**Note:** This script will transition VMs being backed up by Veeam into WT mode
+before backup and transition them back to WB mode when done.
 
 ### Usage
+1.  Create a Directory on the Veeam Server called `c:\veeamprnx`
+2.  Copy the two script file into that directory
+3.  Edit `VeeamPrnxCacheControl.ps1` and edit the following lines:
 
-* Download zip file or clone this repository to your machine.
-* Edit VeeamPrnxCacheControl.ps1 to include the username and password for connecting to vCenter and the PernixData management server.
-* Temporary files will be stored in c:\temp. Change this if necessary.
-* Edit each Veeam Backup & Replication job, select Storage, Advanced, then Advanced again.
-* In the pre-job script field, enter C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe -Command C:\<FOLDER_WHERE_YOU_STORED_SCRIPT>\VeeamPrnxCacheControl.ps1 -JobName 'Your Veeam Job Name Here' -Mode WriteThrough
-* In the post-job script field, enter C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe -Command C:\<FOLDER_WHERE_YOU_STORED_SCRIPT>\VeeamPrnxCacheControl.ps1 -JobName 'Your Veeam Job Name Here' -Mode WriteBack
+        $fvp_server = "FVP_SERVER_NAME"
+        $vcenter = "VCENTER_NAME"
+        $username = "domain\logon"
+
+4.  Enter the correct values for these items. Remember the `$username` needs to
+    be the logon you created for PernixData to install / run
+5.  Save the file.
+6.  Use the Powershell line below to create the $passwordfile credential to be
+    used later.
+
+    **NOTE THAT THE KEY TO THIS FILE IS CONTAINED IN THE SCRIPT AND CAN BE EASILY
+    DECRYPTED BY USERS WITH ACCESS TO BOTH THE SCRIPT AND THE PASSWORD FILE. IT
+    IS RECOMMENDED TO RUN THE VSC SERVICE AS A NAMED USER AND USE NTFS ACCESS
+    CONTROLS TO LIMIT ACCESS TO THE FILES:**
+
+    `Read-Host -AsSecureString -prompt "Enter password" | ConvertFrom-SecureString -key $([Byte[]](1..16))| Out-File fvp_enc_pass.txt `
+
+7.  A new file called `fvp_enc_pass.txt` will be created. This contains an
+    encrypted copy of the password and will be used by the script to run.
+8.  Next, open Veeam and edit the backup job.
+9.  Go to the section on pre and post scripts and enter the following strings
+    in the boxes:
+    - Pre Job:
+        `C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe -Command
+        C:\veeamprnx\backup-veeam.ps1 -Mode WriteThrough`
+    - Post Job:
+        `C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe -Command
+        C:\veeamprnx\backup-veeam.ps1 -Mode WriteBack`
 
 ## Veeam Active Backup Copy
 
--- VeeamPrnxCacheControl.ps1
+++ VeeamPrnxCacheControl.ps1
@@ -1,155 +1,289 @@
 Param ( 
-    [Parameter(Mandatory=$true)][string]$JobName,
-    [Parameter(Mandatory=$true)][ValidateSet('WriteBack', 'WriteThrough')][string]$Mode
+     [Parameter(Mandatory=$true)][ValidateSet("WriteBack", "WriteThrough")][string]$Mode
 )
-Clear-Host
-
-if ( (Get-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue) -eq $null ) {
-    Add-PsSnapin VMware.VimAutomation.Core
-} else {
-    throw 'Could not find vSphere PowerCLI'
+Function Writelog
+{
+  Param ([string]$logstring)
+   $logstring = "$(Get-Date -format s) - " + $logstring
+   $logstring | out-file -Filepath $logfile -append
 }
 
-if ( (Get-PSSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue) -eq $null ) {
-    Add-PsSnapin VeeamPSSnapIn
-} else {
-    throw 'Could not find Veeam PowerShell snapin'
+Function Timer ($timeoutseconds, $vmid)
+{
+ $timeout = new-timespan -Seconds $timeoutseconds
+            $sw = [diagnostics.stopwatch]::StartNew()
+            while ($sw.elapsed -lt $timeout){
+                $vmstatus = Get-PrnxAccelerationPolicy -uuid $VMID -Effective
+                if ($vmstatus = "WRITE_THROUGH"){
+                    writelog "Transition Complete"
+                    return
+                    }
+                else {start-sleep -seconds 1}
+            }
+            writelog "Timed out"
 }
 
-$job = Get-VBRJob -Name $JobName
+#Set these required parameters
+#
+$fvp_server = "Name of FVP Management Server"
+$vcenter = "Name of vCenter"
+$username = "domain\userid"
+
+Add-PSSnapin VMware.VimAutomation.Core -ErrorAction Stop
+Add-PSSnapin VeeamPSSnapIn -ErrorAction Stop
+
+#This bit pulls the process id of the Veeam session. From that it gets the job command used to start the job and then uses the GUID of the job to get the name of the job
+
+$parentpid = (Get-WmiObject Win32_Process -Filter "processid='$pid'").parentprocessid.ToString()
+
+$parentcmd = (Get-WmiObject Win32_Process -Filter "processid='$parentpid'").CommandLine
+
+$veeamjob = Get-VBRJob | ?{$parentcmd -like "*"+$_.Id.ToString()+"*"}
+
+$jobname=$veeamjob.name
+
+$prnxVMs = ""
+
+# Initialize variables
+$logfilepath = "C:\veeamprnx"
+$logfile = $logfilepath + "\"+$jobname+"_fvp_$(get-date -f yyyy_MM_dd_HH_mm_ss).log"
+$passwordfile = $logfilepath + "\fvp_enc_pass.txt"
+
+#Here we get the encrypted password from the password file and use a key to decrypt. You need this so it can be used by the Veeam service running the jobs
+
+    Writelog "Retrieving encrypted password from file $passwordfile"
+    Try { 
+           [Byte[]] $key = (1..16)
+
+           $enc_pass = Get-Content $passwordfile | ConvertTo-SecureString -Key $key
+           Writelog $enc_pass
+       }
+    Catch { 
+           Writelog "Error retrieving encrypted password"
+          Exit 1
+       }
+
+    Try { 
+           $credential = New-Object System.Management.Automation.PsCredential($username, $enc_pass)
+      }
+    Catch {
+           Writelog "Error creating credential object"
+           Exit 1
+      }
 
 # Verify the job exists
+$job = Get-VBRJob -Name $JobName
+
+Writelog "Verifying that the job exists, "$job
 if (!$job) {
-    Write-Error 'Backup job not found!'
+    Writelog "Backup job $jobname not found!"
     Exit 2
 }
 
-$SettingsFile = 'C:\Temp\Job.'+$job.TargetFile+'.Settings.csv'
+$SettingsFile = $logfilepath + "\Job."+$job.TargetFile+".Settings.csv"
 
 # Running some initial tests
-if ($Mode -eq 'WriteThrough') {
-    if (Test-Path $SettingsFile) {
-        Write-Error "It seems the script was not properly stopped before this job run. Review $SettingsFile and perform manual clean-up."
-        Exit 2
-    } else {
-        Write-Host "Create the peer settings file: $SettingsFile"
-        $SettingsFileHandle = New-Item $SettingsFile -Type File
+    if ($Mode -eq "WriteThrough") {
+        if (Test-Path $SettingsFile) {
+            Writelog "Warning: $SettingsFile still exists from previous run. Manually remove and re-run the job"
+            Exit 2
+        } else {
+            Writelog "Created peer settings file: $SettingsFile"
+            $SettingsFileHandle = New-Item $SettingsFile -Type File
+        }
+    } elseif ($Mode -eq "WriteBack") {
+        if (Test-Path $SettingsFile) {
+            Writelog "Transitioning VMs to write back."
+            $SettingsFileHandle = Get-Content $SettingsFile
+        } else {
+            Writelog "Nothing to change, normal exit"
+            Exit 0
+        }
     }
-} elseif ($Mode -eq 'WriteBack') {
-    if (Test-Path $SettingsFile) {
-        Write-Host 'Now we just have to revert the acceleration mode.'
-        $SettingsFileHandle = Get-Content $SettingsFile
-    } else {
-        Write-Host "If we want to stop, but there are no settings to be reverted, everything's just fine..."
-        Exit 0
-    }
-}
 
 # It's showtime!
 
-if ($Mode -eq 'WriteThrough') {
-    Write-Host 'Connecting to VMware vCenter Server'
-    $vmware = Connect-VIServer -Server vcenter -User root -Password vmware -WarningAction SilentlyContinue
-    Write-Host 'Connected to VMware vCenter Server'
+if ($Mode -eq "WriteThrough") {
+  #  Writelog "Connecting to VMware vCenter Server: $vcenter"
+    $vmware = Connect-VIServer -Server $vcenter -credential $credential
+    $objects = $job.GetObjectsInJob() 
 
-    'Getting objects in backup job'
-    $objects = $job.GetObjectsInJob() | Where-Object{$_.Type -eq 'Include'}
-    $excludes = $job.GetObjectsInJob() | Where-Object{$_.Type -eq 'Exclude'}
 
-    # Initiate empty array for VMs to exclude
-    [System.Collections.ArrayList]$es = @()
+Writelog "Building list of included objects."
+# Initiate empty array for VMs to include
+[System.Collections.ArrayList]$is = @()
 
-    'Building list of excluded job objects'
-    foreach ($e in $excludes) {
-        $e.Name
 
-        # If the object added to the job is not a VM, find the contained VMs
-        $view = Get-View -ViObject $e.Name | Get-VIObjectByVIView
-        if ($view.GetType().Name -ne 'VirtualMachineImpl') {
-            foreach ($vm in ($view | Get-VM)) {
-                $i = $es.Add($vm.Name)
-            }
-        } else {
-            $i = $es.Add($view.Name)
-        }
+# This bit looks at the Veeam job and works out if it contains a list of VM's or is a selection based on a container. 
+# It checks for any exclusions if it is a container and then buils a list of VM that are going to be backed up.
 
-    }
+writelog""
+writelog "List of VM's in Backup Job"
+writelog ">>>>>>>>>>>>>>>>>>>>"
+foreach ($o in $objects) {
 
-    'Building list of included objects'
-    # Initiate empty array for VMs to include
-    [System.Collections.ArrayList]$is = @()
-
-    foreach ($o in $objects) {
-        $o.Name 
-
-        # If the object added to the job is not a VM, find the contained VMs
-        $view = Get-View -ViObject $o.Name | Get-VIObjectByVIView
-        if ($view.GetType().Name -ne 'VirtualMachineImpl') {
-            foreach ($vm in ($view | Get-VM)) {
-                if ($es -notcontains $vm.Name) {
-                    $i = $is.Add($vm.Name)
-                }
-            }
-        } else {
-            $i = $is.Add($o.Name)
-        }
-    }
-    Write-Host 'Connecting to PernixData FVP Management Server'
-
-    Import-Module PrnxCLI -ErrorAction SilentlyContinue
-    $prnx = Connect-PrnxServer -NameOrIPAddress localhost -UserName root -Password vmware
-
-    Write-Host 'Getting list of included, powered on VMs with PernixData write-back caching enabled'
-    $prnxVMs = Get-PrnxVM | Where-Object {($_.powerState -eq 'poweredOn') -and ($_.effectivePolicy -eq '7')} | Where-Object -property $_.Name -in -value $is
-    
-    foreach ($vm in $prnxVMs) {
-        if ($vm.numWbExternalPeers -eq $null) {
-            $ext_peers = 0
-        } else {
-            $ext_peers = $vm.numWbExternalPeers
-        }
-
-        $VMName = $vm.Name
-        $VMWBPeers = $vm.NumWBPeers
-        $VMWBExternalPeers = $ext_peers
-
-        $WriteBackPeerInfo = @($VMName,$VMWBPeers,$VMWBExternalPeers)
-        $WriteBackPeerInfo -join ',' | Out-File $SettingsFile -Append
-
-        Write-Host "Transitioning $VMName (peers: $VMWBPeers, external: $VMWBExternalPeers) into writethrough"
+              if( $o.object.vitype -eq "Tag") {$iq = (Get-VM -Tag $o.name) 
+          foreach ($vminlist in $iq){if ((Get-VBRJobObject -Job $jobname -Name $vminlist.name).Object -notmatch "Name:*"){ $i = $is.Add($vminlist.PersistentId)}
+            writelog $vminlist.name
+            write-host $vminlist.name
             
-        Try { 
-            $CacheMode = Set-PrnxAccelerationPolicy -Name $VMName -WriteThrough -ea Stop
-        }
-        Catch {
-            Write-Error "Failed to transition $VMName : $($_.Exception.Message)"
-            Exit 2
-        }
+            }
+          }
+
+          elseif( $o.object.vitype -eq "Datastore") {$iq = (Get-VM -Datastore $o.name) 
+          foreach ($vminlist in $iq){if ((Get-VBRJobObject -Job $jobname -Name $vminlist.name).Object -notmatch "Name:*"){ $i = $is.Add($vminlist.PersistentId)}
+            writelog $vminlist.name
+            write-host $vminlist.name
+            }
+          }
+
+        elseif( $o.object.vitype -ne "VirtualMachine") {$iq = (Get-VM -Location $o.name) 
+          foreach ($vminlist in $iq){if ((Get-VBRJobObject -Job $jobname -Name $vminlist.name).Object -notmatch "Name:*"){ $i = $is.Add($vminlist.PersistentId)}
+            writelog $vminlist.name
+            write-host $vminlist.name
+            $me = Get-VBRJobObject -Job $jobname -Name $vminlist.name
+            write-host $me.object
+           }
+          }
+        
+        elseif ($o.type -ne "Exclude"){ $vv = (Get-VM -name $o.name) 
+                $i = $is.Add($vv.PersistentID)  
+          writelog $vv.name
+          write-host $vv.name        
+         }
+                 
+         Else{ Write-host ""}
+
+ }
+ 
+writelog ">>>>>>>>>>>>>>>>>>>>"
+writelog ""
+Writelog "Connecting to PernixData Management Server: $fvp_server"
+
+Try {
+        import-module prnxcli -ea Stop
+        $prnx = Connect-PrnxServer -NameOrIpAddress $fvp_server -credentials $credential -ea Stop > $null
+        $FVPversion=""
+        $license = Get-PrnxObject -Type PrnxMServer
+        if($license.buildinfo -gt "3.1"){$FVPversion="3.1"}
+        writelog $FVPversion
+    }
+Catch {
+        Writelog "Error connecting to FVP Management Server: $($_.Exception.Message)"
+        exit 1
+    }
+
+Writelog "Connected to PernixData Management Server: $fvp_server"
+Writelog "Getting list of included, powered on VMs with PernixData write back enabled."
+writelog""
+
+$prnxVMs = Get-PrnxVM | Where {($_.powerState -eq "poweredOn") -and ($_.effectivePolicy -eq "7")} | Where { $is -contains $_.UUID }
+
+writelog "*************"
+Writelog "VM That will be transitioned to WriteThrough"
+writelog ">>>>>>>>>>>>>>>>>>>>"
+
+foreach ($vm in $prnxVMs) { writelog $vm.Name }
+writelog ">>>>>>>>>>>>>>>>>>>>"
+writelog""
+writelog "Now we will transition them"
+
+writelog "==========================="
+
+foreach ($vm in $prnxVMs) { 
+
+    writelog "--------------------"
+    writelog $vm.name
+
+    if ($vm.numWbExternalPeers -eq $null) {
+        $ext_peers = 0
+    } else {
+        $ext_peers = $vm.numWbExternalPeers
     }
     
-} elseif ($Mode -eq 'WriteBack') {
-    Write-Host 'Connecting to PernixData FVP Management Server'
+    $VMName = $vm.Name
+    $VMWBPeers = $vm.NumWBPeers
+    $VMWBExternalPeers = $ext_peers
+    $VMID = $VM.uuid
 
-    Import-Module PrnxCLI -ErrorAction SilentlyContinue
-    $prnx = Connect-PrnxServer -NameOrIPAddress localhost -UserName root -Password vmware
+    $WriteBackPeerInfo = @($VMName,$vmid,$VMWBPeers,$VMWBExternalPeers)
+    $WriteBackPeerInfo -join ',' | Out-File $SettingsFile -Append
 
-    foreach ($vm in $SettingsFileHandle) {
-        $VMName            = $vm.split(',')[0]
-        $VMWBPeers         = $vm.split(',')[1]
-        $VMWBExternalPeers = $vm.split(',')[2]
-
-        Write-Host "Transitioning $VMName into writeback mode with $VMWBPeers peers and $VMWBExternalPeers external peers"
-            
-        Try { 
-            $CacheMode = Set-PrnxAccelerationPolicy -Name $VMName -WriteBack -NumWBPeers $VMWBPeers -NumWBExternalPeers $VMWBExternalPeers -ea Stop
-        }
-        Catch {
-            Write-Error "Failed to transition $VMName : $($_.Exception.Message)"
-            Exit 2
-        }
-    }
     
-    Remove-Item -Path $SettingsFile
+    Writelog "Transitioning $VMName (peers: $VMWBPeers, external: $VMWBExternalPeers) into WriteThrough mode."
+
+# Here we are going to transition the VM to WriteTrhough. I have added a test to check the VM has an effective status of Writethrough before mvoing on. It is there to catch any VM's that have a lot of WB data in the cache
+# It is Fairly simple, it grabs the VM UUID, gets the time_to_destage value and then sets this to a time out variable. Once the WT policy has been sent it goes into a loop checking the Effective Status
+# WHen this is WriteThrough it steps on. It uses a timeout of the Time_to_destage + 10 secs so if the VM does not transition for some reason, the loop will stop. 
+        
+    Try { 
+    
+    if($FVPversion -eq "3.1"){$timetodisk = Get-PrnxObjectStats -ObjectIdentifier $VMID -starttime 0 -NumberOfSamples 1}
+    else{$timetodisk = Get-PrnxObjectStats -ObjectIdentifier $VMID -NumberOfSamples 1}
+      
+        writelog $vmid
+   
+    $timeoutseconds = $timetodisk.time_to_destage
+    $timeoutseconds = $timeoutseconds + 10
+    writelog $timeoutseconds
+   
+
+ $CacheMode = Set-PrnxAccelerationPolicy -uuid $VMID -WriteThrough -ea Stop
+
+        #   timer $timeoutseconds $VMID
+        
+    }
+    Catch {
+        Write-Error "Failed to transition $VMName : $($_.Exception.Message)"
+        Exit 2
+    } 
+}
+
+
+
+} elseif ($Mode -eq "WriteBack") {
+
+Writelog "Connecting to PernixData FVP Management Server: $fvp_server"
+
+Try {
+        import-module prnxcli -ea Stop
+        $prnx = Connect-PrnxServer -NameOrIpAddress $fvp_server -credentials $credential -ea Stop > $null
+        $licensetype=""
+        $license = Get-PrnxObject -Type PrnxMServer
+        if($license.licensedFeatures -match "FVP_FEATURE_FAULT_DOMAIN"){$licensetype="Enterprise"}        
+    }
+Catch {
+        Writelog "Error connecting to FVP Management Server: $($_.Exception.Message)"
+        Remove-Item -Path $SettingsFile
+        exit 1
+    }
+
+Writelog "Connected to PernixData Management Server: $fvp_server"
+
+foreach ($vm in $SettingsFileHandle) {
+    $VMName            = $vm.split(",")[0]
+    $VMID              = $vm.split(",")[1]
+    $VMWBPeers         = $vm.split(",")[2]
+    $VMWBExternalPeers = $vm.split(",")[3]
+
+    Writelog "Transitioning $VMName into write back mode with $VMWBPeers peers and $VMWBExternalPeers external peers."
+        
+    Try { 
+    if ($licensetype -eq "Enterprise") {
+        $CacheMode = Set-PrnxAccelerationPolicy -uuid $VMID -WriteBack -NumWBPeers $VMWBPeers -NumWBExternalPeers $VMWBExternalPeers -ea Stop
+         }
+         Else{ $CacheMode = Set-PrnxAccelerationPolicy -uuid $VMID -WriteBack -NumWBPeers $VMWBPeers -ea Stop
+        }
+
+    }
+    Catch {
+        Writelog "Failed to transition $VMName : $($_.Exception.Message)"
+        Exit 2
+    }
+}
+
+Remove-Item -Path $SettingsFile
 }
 
 Disconnect-PrnxServer -Connection $prnx > $null
+if ($vmware) { Disconnect-VIServer -Confirm:$false -server $vcenter > $null }
